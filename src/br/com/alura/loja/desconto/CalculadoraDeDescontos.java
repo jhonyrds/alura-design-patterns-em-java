9992f490a053629a85cb53f3a087c8d6c8a33228
jhonyrodrigueszup <jhony.o_rodrigues@hotmail.com>
2021-09-14 19:37:37 -0300

    aplicando o pattern Adapter

--- src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
+++ src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
@@ -1,8 +1,8 @@
 package br.com.alura.loja.desconto;
 
-import java.math.BigDecimal;
-
 import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
 
 public class CalculadoraDeDescontos {
 	
